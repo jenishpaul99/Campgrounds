14ebf672514b154bb02b3dadb72860297207d4da
jenishpaul99 <62162584+jenishpaul99@users.noreply.github.com>
2020-07-27 20:25:29 +0530

    Update campground.js

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,5 @@
 mongoose = require("mongoose");
-mongoose.connect('mongodb://jenishpaul99:Jenish%401999@cluster0-shard-00-00-89oaw.mongodb.net:27017,cluster0-shard-00-01-89oaw.mongodb.net:27017,cluster0-shard-00-02-89oaw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect('mongodb://jenishpaul99:<your password>@cluster0-shard-00-00-89oaw.mongodb.net:27017,cluster0-shard-00-01-89oaw.mongodb.net:27017,cluster0-shard-00-02-89oaw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
 	useNewUrlParser: true,
 	useCreateIndex: true
 }).then(() => {
